4005ba8d5ddabd2f52a84f4dbbaf982ff840cc7c
Raphael Lima <raphael.aolima8@gmail.com>
2016-06-25 17:54:05 -0300

    pt-br for english

--- src/firmware/save.ino
+++ src/firmware/save.ino
@@ -1,15 +1,15 @@
 /*
  * Copyright (C) 2015 Raphael Lima
- * S.A.V.E - Autonomous System for Parked Vehicles
+ * SAVE - Autonomous System for Parked Vehicles
  *
- * This file is part of S.A.V.E.
+ * This file is part of SAVE.
  *
- * S.A.V.E is free software: you can redistribute it and/or modify
+ * SAVE is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * S.A.V.E is distributed in the hope that it will be useful,
+ * SAVE is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
@@ -17,59 +17,59 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
- 
-  #define pinoEchoFrente 13
-  #define pinoTriggerFrente 12
-  #define pinoEchoTras 11
-  #define pinoTriggerTras 10
+
+  #define echoPinFront 13
+  #define triggerPinFront 12
+  #define echoPinBack 11
+  #define triggerPinBack 10
   #define dip_1 9
   #define dip_2 8
-  #define ledLigado 7
-  #define ledDesligado 6
-  #define ledVidro 5
+  #define ledOn 7
+  #define ledOff 6
+  #define ledWindow 5
   #define buzzer 4
-  
-  int distancia = 0;
-  int estado = 0;
-  int distancias[30];
-  double nivelConfianca = 1.96;
-  double distanciaInicialFrontal = 0;
-  double distanciaInicialTraseira = 0;
-  double distanciaFinalFrontal = 0;
-  double distanciaFinalTraseira = 0;
-  boolean ciclo = true;
-  double intervaloConfianca[2];
-  int distanciaFiltrada[30];
-  
-  void setup() 
+
+  int distance = 0;
+  int state = 0;
+  int distances[30];
+  double confidenceLevel = 1.96;
+  double initialDistanceFront = 0;
+  double initialDistanceBack = 0;
+  double finalDistanceFront = 0;
+  double finalDistanceBack = 0;
+  boolean cicle = true;
+  double confidenceInterval[2];
+  int filtratedDistance[30];
+
+  void setup()
   {
     Serial.begin (9600);
-    pinMode(pinoTriggerTras, OUTPUT);
-    pinMode(pinoEchoTras, INPUT);
-    pinMode(pinoTriggerFrente, OUTPUT);
-    pinMode(pinoEchoFrente, INPUT);
-    pinMode(ledLigado, OUTPUT);
-    pinMode(ledDesligado, OUTPUT);
+    pinMode(triggerPinBack, OUTPUT);
+    pinMode(echoPinBack, INPUT);
+    pinMode(triggerPinFront, OUTPUT);
+    pinMode(echoPinFront, INPUT);
+    pinMode(ledOn, OUTPUT);
+    pinMode(ledOff, OUTPUT);
     pinMode(buzzer, OUTPUT);
-    pinMode(ledVidro, OUTPUT);
+    pinMode(ledWindow, OUTPUT);
     pinMode(dip_1, INPUT_PULLUP);
     pinMode(dip_2, INPUT_PULLUP);
   }
-  
+
   int bin2int(int numValues, ...)
-  { //Primeiro parametro e a quantidade de pinos que serao lidos, os "n" seguintes sao os pinos a serem lidos
+  { //Primeiro parâmetro e a quantidade de pinos que serão lidos, os "n" seguintes são os pinos a serem lidos
     int total = 0;
     va_list values;
     va_start(values, numValues);
-    
+
     for( ; numValues > 0; numValues--)
           if(!(digitalRead(va_arg(values, int))) )
               total += powint(2, numValues-1);
-              
+
     va_end(values);
     return total;
   }
-  
+
   int powint(int base, int exponent)
   {
     int result = 1;
@@ -77,228 +77,228 @@
           result *= base;
     return result;
   }
-  
-  void statusCarro(int acao)
+
+  void carStatus(int action)
   {
-    switch(acao){
+    switch(action){
       case 0:
-        digitalWrite(ledLigado, LOW);
-        digitalWrite(ledDesligado, HIGH);
+        digitalWrite(ledOn, LOW);
+        digitalWrite(ledOff, HIGH);
         break;
-        
+
         case 1:
-          digitalWrite(ledLigado, HIGH);
-          digitalWrite(ledDesligado, LOW);
+          digitalWrite(ledOn, HIGH);
+          digitalWrite(ledOff, LOW);
           break;
     }
   }
-  
-  void acionarAtuadores(boolean atdr)
+
+  void activateActuators(boolean atdr)
   {
-    digitalWrite(ledVidro, HIGH);
+    digitalWrite(ledWindow, HIGH);
     delay(6000);
-    digitalWrite(ledVidro, LOW);
+    digitalWrite(ledWindow, LOW);
       while(atdr) {
         tone(buzzer, 1500);
         delay(300);
         noTone(buzzer);
-        digitalWrite(ledDesligado, LOW);
+        digitalWrite(ledOff, LOW);
         delay(600);
-        digitalWrite(ledDesligado, HIGH);
-        
-        estado = bin2int(2, dip_1,dip_2);
-       
-        if(estado == B010){
+        digitalWrite(ledOff, HIGH);
+
+        state = bin2int(2, dip_1,dip_2);
+
+        if(state == B010){
           atdr = false;
-          ciclo = true;
+          cicle = true;
         }
       }
   }
-  
-   
-  
-  void desligarSistema()
+
+
+
+  void shutdown()
   {
-    int distancia = 0;
-    double distanciaInicialFrontal = 0;
-    double distanciaInicialTraseira = 0;
-    double distanciaFinalFrontal = 0;
-    double distanciaFinalTraseira = 0;
-    boolean ciclo = true;
+    distance = 0;
+    initialDistanceFront = 0;
+    initialDistanceBack = 0;
+    finalDistanceFront = 0;
+    finalDistanceBack = 0;
+    cicle = true;
   }
-  
-  int filtrarValoresDistancia(int distancia[], int tamanho)
+
+  int refineDistanceValues(int distance[], int size)
   {
-        int indice = 0;
-        int media = 0;
-        int soma = 0;
-        int i = 0;      
-        
-        if(tamanho == 10){
-           
-           while(indice < 5){
-               distancia[indice]  =  (distancia[i] + distancia[i+1]) / 2;
-               indice++;
+        int index = 0;
+        int average = 0;
+        int sum = 0;
+        int i = 0;
+
+        if(size == 10){
+
+           while(index < 5){
+               distance[index]  =  (distance[i] + distance[i+1]) / 2;
+               index++;
            }
-           
+
            for(int j = 0; j < 5; j++){
-             soma = soma + distancia[j];
-           }   
-        
-           media = soma / 5;
-           return media;
-        
-        } 
+             sum = sum + distance[j];
+           }
+
+           average = sum / 5;
+           return average;
+
+        }
         else
         {
-          while(indice < 10){
-            distancia[indice]  =  (distancia[i] + distancia[i+1] + distancia[i+2]) / 3;
-            indice++;
+          while(index < 10){
+            distance[index]  =  (distance[i] + distance[i+1] + distance[i+2]) / 3;
+            index++;
             i = i+3;
           }
-             filtrarValoresDistancia(distancia, 10);
+             refineDistanceValues(distance, 10);
         }
   }
-  
-int obterDistancia(int trigger, int echo){
+
+int getDistance(int trigger, int echo){
     for(int i = 0; i < 30; i++){
-        distancias[i] = medirDistancia(trigger, echo);
-        Serial.print(distancias[i]);
+        distances[i] = toMeasureDistance(trigger, echo);
+        Serial.print(distances[i]);
         Serial.print(" | ");
     }
-    int medias = filtrarValoresDistancia(distancias, 30);
-    Serial.print("Media: ");
-    Serial.println(medias);
-    return medias;
+    int averages = refineDistanceValues(distances, 30);
+    Serial.print("Average: ");
+    Serial.println(averages);
+    return averages;
   }
-  
-  double calcularVariancia(int mediasDistancia[], int media){
-    double variancia = 0;
+
+  double calculateVariance(int distancesAverages[], int average){
+    double variance = 0;
     for(int i = 0; i < 30; i++){
-       variancia = variancia + pow((mediasDistancia[i] - media), 2);
-       Serial.print(variancia);
+       variance = variance + pow((distancesAverages[i] - average), 2);
+       Serial.print(variance);
        Serial.print(" | ");
     }
     Serial.println();
     Serial.print("Variancia ");
-    Serial.println(variancia/30);
-    return variancia/30;
+    Serial.println(variance/30);
+    return variance/30;
   }
-  
-  double desvioPadrao(double variancia){  
+
+  double standardDeviation(double variance){
     Serial.print("Desvio Padrao ");
-    Serial.println(sqrt(variancia));
-    return sqrt(variancia);
+    Serial.println(sqrt(variance));
+    return sqrt(variance);
   }
-  
-  void intervaloDeConfianca(int dists[]){
-    int media = filtrarValoresDistancia(dists, 30);
+
+  void confidenceInterval(int dists[]){
+    int average = refineDistanceValues(dists, 30);
     Serial.println("==================================================================================================");
     Serial.print("MEDIA TOTAL: " );
-    Serial.println(media);
-    double variancia = calcularVariancia(dists, media);
-    double desvio = desvioPadrao(variancia);
-    
-    intervaloConfianca[0] = (media - nivelConfianca) * (variancia / desvio);
-    intervaloConfianca[1] = (media + nivelConfianca) * (variancia / desvio);
-  }
-  
-  int calcularMedia(int dist[]){
-    int soma = 0;
-    for(int i = 0; i < 30; i++){
-        soma = soma + dist[i];
-    }
-    return (soma/30);
-  }
-  
-  void loop() 
-  { 
-    estado = bin2int(2, dip_1,dip_2);
+    Serial.println(average);
+    double variance = calculateVariance(dists, average);
+    double deviation = standardDeviation(variance);
 
-    switch(estado){
+    confidenceInterval[0] = (average - confidenceLevel) * (variance / deviation);
+    confidenceInterval[1] = (average + confidenceLevel) * (variance / deviation);
+  }
+
+  int calculateAverage(int dist[]){
+    int sum = 0;
+    for(int i = 0; i < 30; i++){
+        sum = sum + dist[i];
+    }
+    return (sum/30);
+  }
+
+  void loop()
+  {
+    state = bin2int(2, dip_1,dip_2);
+
+    switch(state){
       case(B011)://Dois pinos nivel alto
-          if(ciclo){
-            statusCarro(1);
+          if(cicle){
+            carStatus(1);
             for(int i = 0; i < 30; i++){
-              distanciaFiltrada[i] = obterDistancia(pinoTriggerFrente, pinoEchoFrente);
+              filtratedDistance[i] = getDistance(triggerPinFront, echoPinFront);
               delay(3000);
             }
-            intervaloDeConfianca(distanciaFiltrada);
+            confidenceInterval(filtratedDistance);
             Serial.print("Limite inferior: ");
-            Serial.println(intervaloConfianca[0]);
+            Serial.println(confidenceInterval[0]);
             Serial.print("Limite superior: ");
-            Serial.println(intervaloConfianca[1]);
-            distanciaInicialFrontal = intervaloConfianca[0];
+            Serial.println(confidenceInterval[1]);
+            initialDistanceFront = confidenceInterval[0];
             for(int i = 0; i < 30; i++){
-              distanciaFiltrada[i] = obterDistancia(pinoTriggerTras, pinoEchoTras);
+              filtratedDistance[i] = getDistance(triggerPinBack, echoPinBack);
               delay(3000);
             }
-            intervaloDeConfianca(distanciaFiltrada);
+            confidenceInterval(filtratedDistance);
             Serial.print("Limite inferior: ");
-            Serial.println(intervaloConfianca[0]);
+            Serial.println(confidenceInterval[0]);
             Serial.print("Limite superior: ");
-            Serial.println(intervaloConfianca[1]);
-            distanciaInicialTraseira = intervaloConfianca[0];
-            ciclo = false;
+            Serial.println(confidenceInterval[1]);
+            initialDistanceBack = confidenceInterval[0];
+            cicle = false;
           }
       break;
-  
+
       case(B000): //Dois pinos nivel baixo
-        statusCarro(0);
+        carStatus(0);
         delay(5000); //5 segundos, o ideal seria por volta de 2 minutos.
             for(int i = 0; i < 30; i++){
-              distanciaFiltrada[i] = obterDistancia(pinoTriggerFrente, pinoEchoFrente);
+              filtratedDistance[i] = getDistance(triggerPinFront, echoPinFront);
               delay(3000);
             }
-            intervaloDeConfianca(distanciaFiltrada);
+            confidenceInterval(filtratedDistance);
             Serial.print("Limite inferior: ");
-            Serial.println(intervaloConfianca[0]);
+            Serial.println(confidenceInterval[0]);
             Serial.print("Limite superior: ");
-            Serial.println(intervaloConfianca[1]);
-            distanciaFinalFrontal = intervaloConfianca[0];
+            Serial.println(confidenceInterval[1]);
+            finalDistanceFront = confidenceInterval[0];
             for(int i = 0; i < 30; i++){
-              distanciaFiltrada[i] = obterDistancia(pinoTriggerTras, pinoEchoTras);
+              filtratedDistance[i] = getDistance(triggerPinBack, echoPinBack);
               delay(3000);
             }
-            intervaloDeConfianca(distanciaFiltrada);
+            confidenceInterval(filtratedDistance);
             Serial.print("Limite inferior: ");
-            Serial.println(intervaloConfianca[0]);
+            Serial.println(confidenceInterval[0]);
             Serial.print("Limite superior: ");
-            Serial.println(intervaloConfianca[1]);
-            distanciaFinalTraseira = intervaloConfianca[0];
-            if(distanciaFinalFrontal > distanciaInicialFrontal && distanciaFinalTraseira <= distanciaInicialTraseira){
-                acionarAtuadores(true);
+            Serial.println(confidenceInterval[1]);
+            finalDistanceBack = confidenceInterval[0];
+            if(finalDistanceFront > distanciaInicialFrontal && finalDistanceBack <= initialDistanceBack){
+                activateActuators(true);
             }
       break;
-      
+
       case(B010):
-            desligarSistema();      
+            shutdown();
       break;
-      
+
       case(B001):
 
       break;
-        
+
       }
     delay(1000);
   }
-  
-  int medirDistancia(int trigger, int echo)
+
+  int toMeasureDistance(int trigger, int echo)
   {
     long duration, distance;
-    
+
     digitalWrite(trigger, LOW);  // Added this line
-    
+
     delayMicroseconds(2); // Added this line
-    
+
     digitalWrite(trigger, HIGH);
-    
+
     delayMicroseconds(10); // Added this line
-    
+
     digitalWrite(trigger, LOW);
-    
+
     duration = pulseIn(echo, HIGH);
     distance = (duration/2) / 29.1;
-    
+
     return distance;
   }
